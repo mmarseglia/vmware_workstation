dfc55df97813034b704a24a5c8cf34e8e49f7d5f
Tommy McNeely <thomas.mcneely@davita.com>
2020-04-10 11:30:44 -0600

    Use Facter::Core::Execution

--- lib/facter/vmware_workstation_version.rb
+++ lib/facter/vmware_workstation_version.rb
@@ -4,7 +4,7 @@
 Facter.add('vmware_workstation_version') do
   setcode do
     begin
-      vmware_product_list = system 'vmware-installer -l'
+      vmware_product_list = Facter::Core::Execution.execute('vmware-installer -l')
       vmware_workstation = %r{vmware-workstation.*}.match(vmware_product_list).to_s
       %r{[0-9.]+}.match(vmware_workstation).to_s
     rescue
